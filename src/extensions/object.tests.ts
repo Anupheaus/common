1fab6026d3fb543bf6a833ec7c85b2e289cd4811
Anupheaus <tony.hales@moodinternational.com>
2019-03-01 16:01:31 +0000

    Fixed another bug in Object.merge

--- src/extensions/object.tests.ts
+++ src/extensions/object.tests.ts
@@ -1,4 +1,5 @@
 import './object';
+import './array';
 
 describe.only('merge', () => {
 
@@ -20,6 +21,27 @@ describe.only('merge', () => {
     expect(b.b).to.eq(7);
     b = Object.merge({}, b, a);
     expect(b.b).to.eq(1);
+  });
+
+  it.only('can merge objects and override getters and setters', () => {
+    const a = {
+      a: 1,
+      b: 8,
+    };
+    let b = {
+      a: 7,
+      b: undefined,
+    };
+
+    Object.defineProperty(b, 'b', {
+      get() { return this.a; },
+      enumerable: true,
+      configurable: true,
+    });
+
+    expect(b.b).to.eq(7);
+    b = Object.merge({}, b, a);
+    expect(b.b).to.eq(8);
   });
 
 });
