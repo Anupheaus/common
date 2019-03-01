728c5c87df11af32156fb95c03eacb974b7b2f05
Anupheaus <tony.hales@moodinternational.com>
2019-03-01 14:48:46 +0000

    Fixed bug in Array Extensions

--- src/extensions/array.ts
+++ src/extensions/array.ts
@@ -3,7 +3,7 @@ import { ArrayExtensions, ArrayConstructorExtensions } from './arrayExtensions';
 // tslint:disable:interface-name
 
 Object.extendPrototype(Array.prototype, ArrayExtensions.prototype);
-Object.extendPrototype(Array, ArrayConstructorExtensions);
+Object.extendPrototype(Array, ArrayConstructorExtensions.prototype);
 
 declare global {
   interface Array<T> extends ArrayExtensions<T> { }
