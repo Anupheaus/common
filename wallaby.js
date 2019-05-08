940874cb462c41f0688e368cc34212b041562ee9
Anupheaus <tony.hales@moodinternational.com>
2019-05-08 19:57:36 +0100

    Added wallaby settings to enable createSettings tests

--- wallaby.js
+++ wallaby.js
@@ -2,4 +2,8 @@ const config = require('anux-package/configs/wallaby');
 
 module.exports = config({
   name: 'Anux - Common',
+  include: [
+    { pattern: 'package.json', load: false },
+    { pattern: 'tests/*.json', load: false },
+  ],
 });
