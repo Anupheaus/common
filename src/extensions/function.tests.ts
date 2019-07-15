a79404b7aa24fd2c2decc9b195050f3dd9ada412
Tony Hales <tony.hales@moodinternational.com>
2019-07-16 00:18:40 +0100

    Another fix for the tests

--- src/extensions/function.tests.ts
+++ src/extensions/function.tests.ts
@@ -10,12 +10,11 @@ describe('extensions', () => {
         const stackInfo = Function.getStackTrace();
         expect(stackInfo).to.be.an('array').with.lengthOf(9);
         const currentFrame = stackInfo[0];
-        expect(currentFrame).to.eql({
-          methodName: 'Context.it',
-          file: 'C:\\Users\\tony.hales\\.vscode\\extensions\\wallabyjs.wallaby-vscode-1.0.136\\projects\\c95feb84ff8e9c02\\instrumented\\src\\extensions\\function.tests.js',
-          line: 13,
-          column: 58,
-        });
+
+        expect(currentFrame).to.have.property('methodName', 'Context.it');
+        expect(currentFrame).to.have.property('file').which.is.a('string');
+        expect(currentFrame).to.have.property('line').which.is.a('number');
+        expect(currentFrame).to.have.property('column').which.is.a('number');
       });
 
     });
