2e71bc608890ac49b7b59cd06053263b92ffbe51
Tony Hales <tony.hales@revlifter.com>
2023-02-19 15:20:05 +0000

    Updates to the logger class

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,5 +1,6 @@
 import { BaseLogger, ILogObj, ISettingsParam } from 'tslog';
-import { is } from '../extensions';
+import { Error as CommonError } from '../errors';
+import { AnyObject, is } from '../extensions';
 
 const defaultMinLevel = 5;
 
@@ -34,5 +35,63 @@ export class Logger<T extends ILogObj = ILogObj> extends BaseLogger<T> {
       if (level) return level;
     }
     return defaultMinLevel;
+  }
+
+  public silly(message: string, meta?: AnyObject): void {
+    this.log(0, 'silly', message, meta);
+  }
+
+  public trace(message: string, meta?: AnyObject): void {
+    this.log(1, 'trace', message, meta);
+  }
+
+  public debug(message: string, meta?: AnyObject): void {
+    this.log(2, 'debug', message, meta);
+  }
+
+  public info(message: string, meta?: AnyObject): void {
+    this.log(3, 'info', message, meta);
+  }
+
+  public warn(message: string, meta?: AnyObject): void {
+    this.log(4, 'warn', message, meta);
+  }
+
+  public error(message: string, meta?: AnyObject): void;
+  public error(error: Error, meta?: AnyObject): void;
+  public error(messageOrError: string | Error, meta?: AnyObject): void {
+    let message = '';
+    if (messageOrError instanceof Error) {
+      message = is.string(messageOrError) ? messageOrError : `${messageOrError.name}: ${messageOrError.message}`;
+      const error = messageOrError;
+      meta = {
+        ...meta,
+        ...(error instanceof CommonError ? error : {
+          stack: error.stack,
+        }),
+      };
+    } else {
+      message = messageOrError;
+    }
+    this.log(5, 'error', message, meta);
+  }
+
+  public fatal(message: string, meta?: AnyObject): void;
+  public fatal(error: Error, meta?: AnyObject): void;
+  public fatal(messageOrError: string | Error, meta?: AnyObject): void {
+    let message = '';
+    if (messageOrError instanceof Error) {
+      message = is.string(messageOrError) ? messageOrError : `${messageOrError.name}: ${messageOrError.message}`;
+      const error = messageOrError;
+      meta = {
+        ...meta,
+        ...(error instanceof CommonError ? error : {
+          stack: error.stack,
+        }),
+      };
+    } else {
+      message = messageOrError;
+    }
+    this.log(6, 'fatal', message, meta);
   }
 }
