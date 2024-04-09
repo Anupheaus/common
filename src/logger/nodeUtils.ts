f75206fcf08f8f16f21525ea2510203415dc8de4
Tony Hales <tony.hales@revlifter.com>
2024-04-09 22:54:48 +0100

    Updated logger so that it does not crash if it cannot write out to a file.

--- src/logger/nodeUtils.ts
+++ src/logger/nodeUtils.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import path from 'path';
 import { AnyObject } from '../extensions';
 
 function stripToOnlyPrintableCharacters(str: string) {
@@ -6,8 +7,20 @@ function stripToOnlyPrintableCharacters(str: string) {
   return str.replace(/[^a-zA-Z0-9\s\.,;:<>!@#$%^&*()_+={}\[\]|\-`~\\/?'"<>]/g, '');
 }
 
+let hasRaisedError = false;
+
 export function writeToFile(filename: string, message: string, meta: AnyObject | undefined) {
   message = stripToOnlyPrintableCharacters(message);
   const fullMessage = `${message}\n${meta != null ? `${JSON.stringify(meta, undefined, 2)}\n` : ''}`;
-  fs.writeFileSync(filename, fullMessage, { flag: 'a', mode: 0o666, encoding: 'utf8' });
+  try {
+    if (!fs.existsSync(path.dirname(filename))) {
+      fs.mkdirSync(path.dirname(filename), { recursive: true });
+    }
+    fs.writeFileSync(filename, fullMessage, { flag: 'a', mode: 0o666, encoding: 'utf8' });
+  } catch (err) {
+    if (hasRaisedError) return;
+    hasRaisedError = true;
+    // eslint-disable-next-line no-console
+    console.error(`Error writing to file: ${filename}`, err);
+  }
 }
