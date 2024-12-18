1f44bf8c57e7af5667452af0e7c54a471282b782
Tony Hales <tony.hales@revlifter.com>
2024-12-18 00:41:27 +0000

    Modified data sorts

--- src/models/data/DataSortsModels.ts
+++ src/models/data/DataSortsModels.ts
@@ -1,23 +1,34 @@
-import type { AnyObject } from '../../extensions';
+import { is, type AnyObject } from '../../extensions';
 
 export type DataSortDirection = 'asc' | 'desc';
 
-export type DataSort<T extends AnyObject = AnyObject> = [keyof T, DataSortDirection];
+export type StrictDataSort<T extends AnyObject = AnyObject> = [keyof T, DataSortDirection];
 
-export type DataSorts<T extends AnyObject = AnyObject> = DataSort<T>[];
+export type DataSort<T extends AnyObject = AnyObject> = StrictDataSort<T> | keyof T;
 
-export const DataSortDirections = {
-  toCSSClass(direction: DataSortDirection): string {
+export type DataSorts<T extends AnyObject = AnyObject> = (DataSort<T>[]) | keyof T;
+
+export namespace DataSorts {
+  export function toArray<T extends AnyObject = AnyObject>(sorts?: DataSorts<T>): StrictDataSort<T>[] {
+    if (sorts == null) return [];
+    if (is.array(sorts)) return sorts.map(sort => is.array(sort) ? sort : [sort, 'asc']);
+    return [[sorts, 'asc']];
+  }
+}
+
+export namespace DataSortDirections {
+  export function toCSSClass(direction: DataSortDirection): string {
     switch (direction) {
       case 'asc': return 'is-sorted-asc';
       case 'desc': return 'is-sorted-desc';
       default: return '';
     }
-  },
+  }
 
-  moveToNextState(direction: DataSortDirection): DataSortDirection | undefined {
+  export function moveToNextState(direction: DataSortDirection): DataSortDirection | undefined {
     if (direction === 'asc') return 'desc';
     if (direction === 'desc') return undefined;
     return 'asc';
-  },
-};
+  }
+
+}
